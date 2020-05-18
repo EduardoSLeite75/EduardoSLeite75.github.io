1dda308bf9b69ca77bcf61024aa3c711945acf5d
Eduardo Soares Leite <eduardo.leite@serpro.gov.br>
2020-05-18 18:55:48 -0300

    commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { EmConstrucaoComponent } from './construcao/emConstrucao.component';
+import { PrincipalComponent } from './principal/principal.component';
+import { ContatoComponentComponent } from './contato/contato-component.component';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'principal', component: EmConstrucaoComponent },
+  { path: 'contato', component: EmConstrucaoComponent },
+  { path: '', redirectTo: '/principal', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+
+}
---
+++ src/app/contato/contato-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contato-component',
+  templateUrl: './contato-component.component.html',
+  styleUrls: ['./contato-component.component.css']
+})
+export class ContatoComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

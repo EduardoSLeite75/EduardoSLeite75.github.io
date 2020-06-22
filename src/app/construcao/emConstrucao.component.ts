e3322eb192517c5c544d13f760943566fa2cf6e8
Eduardo Soares Leite <eduardo.leite@serpro.gov.br>
2020-06-21 23:33:57 -0300

    Revert "Auto-generated commit"
    This reverts commit 985e674a26df285a777bfcfe16e3b03bf343e798.

---
+++ src/app/construcao/emConstrucao.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-emconstrucao-component',
+    templateUrl: './emConstrucao.component.html'
+})
+export class EmConstrucaoComponent {
+
+}

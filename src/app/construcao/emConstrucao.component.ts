985e674a26df285a777bfcfe16e3b03bf343e798
Eduardo Soares Leite <eduardo.leite@serpro.gov.br>
2020-06-21 22:55:44 -0300

    Auto-generated commit

--- src/app/construcao/emConstrucao.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-emconstrucao-component',
-    templateUrl: './emConstrucao.component.html'
-})
-export class EmConstrucaoComponent {
-
-}
--- src/app/principal/principal.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'app-principal-component',
-    templateUrl: './principal.component.html'
-})
-export class PrincipalComponent {
-
-}
